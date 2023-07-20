a493c4b7da976ac8f856c592aed1c489bf628b70
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-07-19 21:01:54 -0300

    Adiciona classe ExceptionHandler para tratar os erros da API

---
+++ src/main/java/br/com/alura/jornada_milhas/infra/exceptions/ExceptionsInternalHandler.java
@@ -0,0 +1,23 @@
+package br.com.alura.jornada_milhas.infra.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.OffsetDateTime;
+
+@RestControllerAdvice
+public class ExceptionsInternalHandler {
+
+    @ExceptionHandler(TestimonyNotFoundException.class)
+    public ResponseEntity<ExceptionResponse> handleTestimonyNotFoundException(TestimonyNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ExceptionResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+    }
+
+    private record ExceptionResponse(Integer status, String message, OffsetDateTime timestamp) {
+        public ExceptionResponse(Integer status, String message) {
+            this(status, message, OffsetDateTime.now());
+        }
+    }
+}
