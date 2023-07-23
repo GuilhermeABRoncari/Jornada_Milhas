7cc71fd591b2014ac3c16340c5f782c516dea718
Guilherme Alves Batista Roncari <123610693+GuilhermeABRoncari@users.noreply.github.com>
2023-07-23 19:54:53 -0300

    Alterando idioma das respostas

--- src/main/java/br/com/alura/jornada_milhas/domain/dtos/responses/TestimonyResponseDto.java
+++ src/main/java/br/com/alura/jornada_milhas/domain/dtos/responses/TestimonyResponseDto.java
@@ -1,10 +1,22 @@
 package br.com.alura.jornada_milhas.domain.dtos.responses;
 
 import br.com.alura.jornada_milhas.domain.entitys.Testimony;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.OffsetDateTime;
 
-public record TestimonyResponseDto(String id, String author, String testimony, String profilePicture, Boolean edited, OffsetDateTime postTime) {
+public record TestimonyResponseDto(
+        String id,
+        @JsonProperty("autor")
+        String author,
+        @JsonProperty("depoimento")
+        String testimony,
+        @JsonProperty("foto")
+        String profilePicture,
+        @JsonProperty("editado")
+        Boolean edited,
+        @JsonProperty("postado_as")
+        OffsetDateTime postTime) {
     public TestimonyResponseDto(Testimony testimony) {
         this(testimony.getId(), testimony.getAuthor(), testimony.getDescription(), testimony.getProfilePicture(), testimony.getEdited(), testimony.getPostTime());
     }
